c9e76e3275404cc9dca483b839703448ef5d6ec0
Asif Ahmed Neloy <asif.neloy@northsouth.edu>
2017-07-29 21:27:58 +0600

    Add files via upload

---
+++ Motor_Check.ino
@@ -0,0 +1,26 @@
+#define leftMotF 4
+#define leftMotB 5
+#define leftMotRPM 6
+
+#define rightMotF 2
+#define rightMotB 3
+#define rightMotRPM 7
+
+void setup() {
+ Serial.begin(9600);
+  pinMode(leftMotF,OUTPUT);
+  pinMode(leftMotB,OUTPUT);
+  pinMode(leftMotRPM,OUTPUT);
+  pinMode(rightMotF,OUTPUT);
+  pinMode(rightMotB,OUTPUT);
+  pinMode(rightMotRPM,OUTPUT);
+}
+
+void loop() {  
+  analogWrite(leftMotRPM, 0);
+  analogWrite(rightMotRPM, 255);
+  digitalWrite(leftMotF , HIGH);
+  digitalWrite(leftMotB , LOW);
+  digitalWrite(rightMotF , HIGH);
+  digitalWrite(rightMotB , LOW); 
+}
